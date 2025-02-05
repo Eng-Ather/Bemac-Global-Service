43603249ecb960715993c8430c0fbc1838c1b049
Ather Ali Siddiqui <engr.ather.93@gmail.com>
2025-02-05 05:09:32 +0500

    edit service page

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,10 @@
 const nextConfig = {
 
     images: {
-        domains: ['economics.illinois.edu','via.placeholder.com','workdesign.com'],
+        domains: ['economics.illinois.edu','via.placeholder.com','workdesign.com',
+          'i.pinimg.com', 'lh7-rt.googleusercontent.com', 'pathwaycentres.oxfordinternational.com',
+        'encrypted-tbn0.gstatic.com', 'encrypted-tbn0.gstatic.com', 'encrypted-tbn0.gstatic.com'
+      ,'imageio.forbes.com', 'encrypted-tbn0.gstatic.com'],
       },
 };
 
--- src/app/Services/page.tsx
+++ src/app/Services/page.tsx
@@ -27,9 +27,9 @@ const ServiceCard = ({ title, description, imgSrc }: { title: string, descriptio
 const ServicesPage = () => {
     return (
         <div>
-            
+
             {/* Services Description */}
-            <div className="py-10 max-w-7xl mx-auto px-6">
+            <div className="py-10 w-full mx-auto px-4 md:px-10">
                 <h2 className="text-3xl font-bold text-center text-red-900 mb-8">
                     Our Expertise at Bemac Global Services
                 </h2>
@@ -39,36 +39,49 @@ const ServicesPage = () => {
                 </p>
 
                 {/* Service Cards */}
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 
+                 gap-8">
                     <ServiceCard
-                        title="Recruitment"
+                        title="Manpower Recruitment & Training"
                         description="We provide top-tier recruitment services, helping businesses find the best talent. Our recruitment solutions are designed to cater to a wide range of industries and job levels."
-                        imgSrc="https://via.placeholder.com/500"
+                        imgSrc="https://i.pinimg.com/1200x/ab/0d/af/ab0dafc18a9d5114434623141eff96ce.jpg"
                     />
                     <ServiceCard
-                        title="Training"
-                        description="Our training services are designed to empower employees with the necessary skills to thrive in their roles. We offer both in-person and virtual training solutions."
-                        imgSrc="https://via.placeholder.com/500"
+                        title="Credential Evaluation"
+                        description="We provide a comprehensive credential evaluation service to assess and verify academic and professional qualifications, ensuring global recognition."
+                        imgSrc="https://lh7-rt.googleusercontent.com/docsz/AD_4nXfDg9bgoyDnM5useDBXAi6si_r3bakajC2E9AW7hdM7HGoHQijf88EWW_FFGgm66Kcrbs2aC_41QWKIch0gmaqbCmH9Fx6WBtRPm4DXkQe0hhhrAzMwXy2eN3pgvCv6k-jb8vlB-5Ghx4F5id0wODLlOKZP?key=rIivelhjQuTO-3ukXd3_mw"
                     />
                     <ServiceCard
-                        title="Education"
+                        title="International Education & Admissions"
                         description="We offer a wide range of educational services to help individuals and organizations stay ahead. Our educational programs are tailored to provide the best learning experience."
-                        imgSrc="https://via.placeholder.com/500"
+                        imgSrc="https://pathwaycentres.oxfordinternational.com/wp-content/uploads/2024/02/DSC00444-scaled.jpg"
                     />
                     <ServiceCard
-                        title="Investment"
+                        title="Banking, Finance & Investment"
                         description="Our investment services help businesses make smart decisions in various sectors. We provide insights and strategies to maximize your returns and secure a stable future."
-                        imgSrc="https://via.placeholder.com/500"
+                        imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcToLf_WLANhbUwj71yWj5JkwtgYPgutlmSdXg&s"
                     />
                     <ServiceCard
                         title="E-Commerce"
                         description="We specialize in helping businesses establish and grow their e-commerce presence. From website design to sales strategies, our team ensures your business thrives online."
-                        imgSrc="https://via.placeholder.com/500"
+                        imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQBQ3gBV8dXKfly11V5CNmZy9TNpIkRWD8zuQ&s"
                     />
                     <ServiceCard
-                        title="Consulting"
-                        description="Our consulting services offer expert advice on various aspects of business growth. We help companies improve their processes, strategies, and overall performance."
-                        imgSrc="https://via.placeholder.com/500"
+                        title="Global Contracting"
+                        description="We provide comprehensive global contracting solutions, ensuring seamless international agreements, compliance, and efficient project execution."
+                        imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGiChYOnk585WU7Gp6W361QTWwPA_8G0QNkg&s"
+                    />
+
+                    <ServiceCard
+                        title="International Volunteer Placement"
+                        description="We connect passionate volunteers with meaningful opportunities worldwide, helping them make a positive impact in communities across the globe."
+                        imgSrc="https://imageio.forbes.com/specials-images/imageserve/65ad49201b8a4682439df59f/Happy-group-of-volunteer-people-showing-importance-of-volunteering-/960x0.jpg?format=jpg&width=960"
+                    />
+
+                    <ServiceCard
+                        title="Immigration Services"
+                        description="We offer expert immigration services, assisting individuals and businesses with visa applications, work permits, residency, and citizenship processes."
+                        imgSrc="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRg65g84xtBuNjEa8v2qE0XrJZ0DvqKmYiESg&s"
                     />
                 </div>
             </div>
